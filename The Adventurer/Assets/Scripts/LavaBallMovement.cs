2e142181402a4e431459a71be3f49e63cc25249f
JoeCrocker <josephcrocker14@hotmail.com>
2018-03-05 11:44:29 +1100

    Minor AI Update

--- The Adventurer/Assets/Scripts/LavaBallMovement.cs
+++ The Adventurer/Assets/Scripts/LavaBallMovement.cs
@@ -6,9 +6,11 @@ public class LavaBallMovement : MonoBehaviour
 {
     public float Speed;
     public float TravelTime;
+    private CharacterController Controller;
 
 	void Start () 
     {
+        Controller = GetComponent<CharacterController>();
 	}
 	
 	void Update () 
@@ -16,7 +18,8 @@ public class LavaBallMovement : MonoBehaviour
         TravelTime -= 1 * Time.deltaTime;
         if (TravelTime > 0)
         {
-            this.gameObject.transform.position += transform.forward * Speed * Time.deltaTime;
+            this.transform.Rotate(0, 3, 0);
+            Controller.Move(new Vector3(0, 0, -Speed) * Time.deltaTime);
         }
         else if (TravelTime <= 0)
         {
